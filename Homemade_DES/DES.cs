f3f4f347fd9f27c1d2ddf257714f56d479431099
Паша Ярошенко <pavlo.legominecraft@gmail.com>
2024-09-06 21:17:23 +0300

    Добавьте файлы проекта.

---
+++ Homemade_DES/DES.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homemade_DES
+{
+    internal class DES
+    {
+        private byte[] key;
+        private byte[] text;
+        public DES(byte[] key,byte[] text) {
+            if(key.Length > 7 )
+            {
+                throw new Exception("Mngog bitov");
+            }
+            else
+            {
+                this.key = key;
+            }
+            if (text.Length > 8)
+            {
+                throw new Exception("Mngog bitov text");
+            }
+            else
+            {
+                this.text = text;
+            }
+        }
+
+        public void Encoding()
+        {
+
+        }
+        public void Decoding() 
+        { 
+            
+        }
+        private int[] CreateKeys()
+        {
+            return new int[] { 1, 0, 1, 0, 1, 0, 1, 0 };
+        }
+    }
+}
---
+++ Homemade_DES/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace Homemade_DES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            //DES ds = new DES();
+        }
+    }
+}
